ce7ab8f7ac45adf15a0cb67cfe5ad94d675426bb
井0A <s60046s60046@gmail.com>
2026-09-07 04:08:13 +0800

    Compose Hydra I headless gameplay runtime

--- js/core/game.js
+++ js/core/game.js
@@ -1,6 +1,11 @@
 import { GameClock } from './clock.js';
 import { EventBus } from './event-bus.js';
 import { GameStateStore, createInitialState } from './state.js';
+import { createHydraIRule } from '../math/hydra-rules.js';
+import { createHydraRegrowthSystem } from '../systems/hydra-regrowth.js';
+import { createCombatSystem } from '../systems/combat.js';
+import { createAutoSlashSystem } from '../systems/auto-slash.js';
+import { createManualAttackInput } from '../input/manual-attack.js';
 
 export function createCoreRuntime({
   fixedStepMs = 100,
@@ -44,6 +49,42 @@ export function createCoreRuntime({
       clock.stop();
       unsubscribeClock();
       events.clear();
+    },
+  };
+}
+
+export function createHydraIGameRuntime({
+  fixedStepMs = 100,
+  regenDelayMs = 1500,
+  initialState = createInitialState(),
+} = {}) {
+  const core = createCoreRuntime({ fixedStepMs, initialState });
+  const rule = createHydraIRule({ regenDelayMs });
+
+  const regrowth = createHydraRegrowthSystem(core);
+  const combat = createCombatSystem({
+    ...core,
+    getRule: () => rule,
+  });
+  const autoSlash = createAutoSlashSystem(core);
+  const manual = createManualAttackInput(core);
+
+  return {
+    ...core,
+    rule,
+    manualAttack(options) {
+      return manual.attack(options);
+    },
+    systems: Object.freeze({
+      regrowth,
+      combat,
+      autoSlash,
+    }),
+    destroy() {
+      autoSlash.destroy();
+      combat.destroy();
+      regrowth.destroy();
+      core.destroy();
     },
   };
 }
